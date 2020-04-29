de7800a319fc2ccd5d3406add5baf6d7597adc49
Anthony Dela Cruz <anthony@pushfit.tv>
2020-04-29 19:02:51 +0800

    returned uncommented title

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -13,7 +13,7 @@ export default function Post({ postDetails }) {
           </Link>
         </div>
         <div className="sample-img mb-4"></div>
-        {/* <h1>{postDetails.title}</h1> */}
+        <h1>{postDetails.title}</h1>
         <div>
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
